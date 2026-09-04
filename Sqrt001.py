8c68dd28cef5788a5f1821de8ae77a6d28993106
huiyun2801 <huiyun2103@gmail.com>
2026-09-04 19:00:36 +0800

    Update Sqrt001.py

--- Sqrt001.py
+++ Sqrt001.py
@@ -1,4 +1,11 @@
-x = 1
-t = 1
+d = x = 1.000
+t = 0
 
 y = float(input("Enter a number:"))
+
+while d>0.01 or d<-0.01 or t<51 :
+    t+=1
+    d = y/x-x
+
+    if d>=-0.01 and d<=0.01 :
+        print("Squre root is", x)
